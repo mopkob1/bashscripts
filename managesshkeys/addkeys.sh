49d2428a846892b9a17806e10544c3d258e6018d
mopkob <mopkob@ituse.ru>
2016-05-25 19:33:25 +0300

    Убрал ошибку отсуствия файла с ключем

--- managesshkeys/addkeys.sh
+++ managesshkeys/addkeys.sh
@@ -2,7 +2,7 @@
 FILE="allinall"
 grep -E -v "(^#|#$)" "./list" |\
     while read ADDR; do
-	OWN=$(cat "./$ADDR/id_rsa.pub")
+	OWN=$(cat "./$ADDR/id_rsa.pub" || echo "ERROR")
 	grep -E -v "(^#|#$)" "$FILE" >> "./$ADDR/$FILE.tmp" 
 	cat "./$ADDR/authorized_keys" >> "./$ADDR/$FILE.tmp" 
 	cat "./$ADDR/$FILE.tmp" | sort | uniq | grep -v "$OWN" > "./$ADDR/authorized_keys"
