eacd314a94471a427972578cf97b3f3403df6cb0
mopkob <mopkob@ituse.ru>
2016-08-01 01:06:59 +0300

    Revert "Поправил управление ssh ключами. Теперь неактивные хосты - отбразываются"
    This reverts commit db9b103d44166ba433fa6cdec8b258e91f036858.

--- managesshkeys/setkeys.sh
+++ managesshkeys/setkeys.sh
@@ -3,5 +3,5 @@
 grep -E -v "(^#|#$|^$)" "./list" |\
     while read ADDR; do
 	echo "Sync with $ADDR"
-	ping -c 3 "$ADDR" > /dev/null  && rsync -avr --exclude="*.bak" "./$ADDR/" "$ADDR:/root/.ssh/" 
+	rsync -avr --exclude="*.bak" "./$ADDR/" "$ADDR:/root/.ssh/" 
     done
